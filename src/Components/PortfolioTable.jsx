32970665f687904b06c38583325ef81925d3e578
FacundoBe <facundobellomo@gmail.com>
2025-02-25 16:56:57 -0300

    agregado margen en las tarjetas por moneda del Portfolio

--- src/Components/PortfolioTable.jsx
+++ src/Components/PortfolioTable.jsx
@@ -9,19 +9,17 @@ export default function PortfolioTable({ coinByWalletList }) {
 
         <div className="portfolio-table-container flex-column w100">
             <div className="portfolio-table-header">
-                <span className="flex">Wallet / Exchange</span>
-                <span className="flex">24h</span>
-                <span className="flex right ">Funds</span>
+                <span className="portfolio-header-left flex">Wallet / Exchange</span>
+                <span className="portfolio-header-right flex right ">Funds</span>
                 <div className="grid-divider">  </div>
             </div>
 
             {coinByWalletList.map((wallet) => (
                 <div key={wallet.wallet} className="portfolio-table-row">
-                    <div className="wallet-name ">
+                    <div className="portfolio-wallet-name ">
                         {wallet.wallet}
                     </div>
-                    <div> 2%</div>
-                    <div className="flex right ">{wallet.amount}</div>
+                    <div className="portfolio-table-funds flex right ">{wallet.amount}</div>
                     <div className="grid-divider">  </div>
                 </div>))}
         </div>
