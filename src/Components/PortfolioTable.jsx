a600b7b0b7b80ee2256ae8188cc38b30be986f62
FacundoBe <facundobellomo@gmail.com>
2025-05-28 17:04:15 -0300

    hecho responsive la seccionde tarjetas de tenecias por wallet en assets

--- src/Components/PortfolioTable.jsx
+++ src/Components/PortfolioTable.jsx
@@ -9,7 +9,7 @@ export default function PortfolioTable({ coinByWalletList, price }) {
 
         <div className="portfolio-table-container flex-column">
             <div className="portfolio-table-header">
-                <span className="portfolio-header-left flex">Wallet / Exchange</span>
+                <span className="portfolio-header-left flex left">Wallet / Exchange</span>
                 <span className="portfolio-header-right flex right ">Funds</span>
                 <span className="portfolio-header-right flex right ">Value</span>
                 <div className="grid-divider">  </div>
@@ -21,7 +21,7 @@ export default function PortfolioTable({ coinByWalletList, price }) {
                         {wallet.wallet}
                     </div>
                     <div className="portfolio-table-funds flex right ">{wallet.amount}</div>
-                    <div className="portfolio-table-value flex right ">{(wallet.amount*price).toFixed(2)} <span>USD</span></div>
+                    <div className="portfolio-table-value flex right "> <p>{(wallet.amount * price).toFixed(2)} <span>USD</span></p></div>
                     <div className="grid-divider">  </div>
                 </div>))}
         </div>
--- src/Components/WalletPieChart.jsx
+++ src/Components/WalletPieChart.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
 import styles from './WalletPieChart.module.css'
-import {COLORS} from '../Assets/constants'
+import { COLORS } from '../Assets/constants'
 
 
- //const COLORS = ["#e9d8a6", "#ee9b00", "#ca6702", "#bb3e03", "#ae2012", "#9b2226", "#005f73", "#0a9396", "#94d2bd",];
+//const COLORS = ["#e9d8a6", "#ee9b00", "#ca6702", "#bb3e03", "#ae2012", "#9b2226", "#005f73", "#0a9396", "#94d2bd",];
 
 export default function WalletPieChart({ data = [], totalValue, ...rest }) {
- 
+
     function Legend() {
 
         return (
@@ -41,28 +41,30 @@ export default function WalletPieChart({ data = [], totalValue, ...rest }) {
     return (
         <div {...rest}>
             <div className={styles['main-container']}>
-                <PieChart width={130} height={100} >
-                    <Pie
-                        data={data}
-                        cx={"50%"}
-                        cy={"50%"}
-                        innerRadius={30}
-                        outerRadius={40}
-                        fill="#1884d8"
-                        paddingAngle={2}
-                        cornerRadius={0}
-                        dataKey="value"
-                        fontSize={"0.8rem"}
-                    >
-                        {data.map((entry, index) => {
-                            return <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} stroke={'transparent'} style={{outline: 'none'}} />
-                        })}
+                <ResponsiveContainer>
+                    <PieChart>
+                        <Pie
+                            data={data}
+                            cx={"50%"}
+                            cy={"50%"}
+                            innerRadius={30}
+                            outerRadius={40}
+                            fill="#1884d8"
+                            paddingAngle={2}
+                            cornerRadius={0}
+                            dataKey="value"
+                            fontSize={"0.8rem"}
+                        >
+                            {data.map((entry, index) => {
+                                return <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} stroke={'transparent'} style={{ outline: 'none' }} />
+                            })}
 
-                        {/*<LabelList dataKey="name" position="outside" fontSize={"0.7rem"} >*/}
+                            {/*<LabelList dataKey="name" position="outside" fontSize={"0.7rem"} >*/}
 
-                    </Pie>
-                    <Tooltip content={<CustomTooltip />} />
-                </PieChart>
+                        </Pie>
+                        <Tooltip content={<CustomTooltip />} />
+                    </PieChart>
+                </ResponsiveContainer>
                 {/* <Legend /> */}
             </div>
         </div>
