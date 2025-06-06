96f47508db8e791be31c8201412af5352ef128f5
FacundoBe <facundobellomo@gmail.com>
2025-06-05 21:33:40 -0300

    agegada la barra de navegacion superior ne version movil

--- src/Components/LayOut.jsx
+++ src/Components/LayOut.jsx
@@ -8,24 +8,27 @@ export default function LayOut() {
 
     const [latBarVisible, setLatBarVisible] = useState(false)
 
-    function hideLatBar(){
+    function hideLatBar() {
         setLatBarVisible(false)
     }
 
     return (
         <div className='layout-container'>
-
-            <svg 
-            className='menu-icon' 
-            onClick={() => {setLatBarVisible(prev => !prev)}}
-            xmlns="http://www.w3.org/2000/svg" 
-            fill="none" 
-            viewBox="0 0 24 24" 
-            strokeWidth="1.5" 
-            stroke="currentColor" >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-
+            <nav className='top-nav-bar'>
+                <div className='top-nav-logo-title-container'>
+                    <img className="logo-criptofolio-top-nav-bar" src="logo-cryptofolio.svg" alt="" />
+                    <p>Portfolio</p>
+                </div>
+                <svg
+                    className='menu-icon'
+                    onClick={() => { setLatBarVisible(prev => !prev) }}
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.8"
+                    stroke="currentColor" >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </nav>
             <LateralBar className={latBarVisible ? "visible" : ""} hideLatBar={hideLatBar} />
             <Outlet />
         </div>
